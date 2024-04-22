76aa66a7414bc60ddc2eabe525b2601ee40895a9
JousenD <romero87_2112@hotmail.com>
2024-04-22 23:22:09 +0200

    3. Input System Implemented

---
+++ Assets/UndeadSurvival2D/Scripts/Input/InputReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace JousenD.UndeadSurvival2D.Input
+{
+    [CreateAssetMenu(fileName = "InputReader", menuName = "Input/Input Reader")]
+    public class InputReader : ScriptableObject
+    {
+        public void OnMove(Vector2 value)
+        {
+            Debug.Log("Moving: " + value);
+        }
+    }
+}
---
+++ Assets/UndeadSurvival2D/Scripts/Input/PlayerInputHandler.cs
@@ -0,0 +1,17 @@
+﻿﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.InputSystem;
+
+namespace JousenD.UndeadSurvival2D.Input
+{
+    public class PlayerInputHandler : MonoBehaviour
+    {
+         [SerializeField]
+        private InputReader _inputReader;
+
+        public void OnMove(InputValue value)
+        {
+            _inputReader.OnMove(value.Get<Vector2>());
+        }
+    }
+}
