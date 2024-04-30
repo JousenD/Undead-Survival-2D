3baba563f1a3ba227d34c673f8e8ff4708177148
JousenD <romero87_2112@hotmail.com>
2024-05-01 00:41:35 +0200

    14. Damage Functionality Implemented

---
+++ Assets/UndeadSurvival2D/Scripts/Character/Damageable.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace JousenD.UndeadSurvival2d.Character
+{
+    public class Damageable : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Debug.Log("Taking Damage of: " + damage);
+        }
+    }
+}
--- Assets/UndeadSurvival2D/Scripts/Enemy/EnemyBehaviour.cs
+++ Assets/UndeadSurvival2D/Scripts/Enemy/EnemyBehaviour.cs
@@ -6,17 +6,49 @@ namespace JousenD.UndeadSurvival2d.Enemy
 {
     public class EnemyBehaviour : CharacterBehaviour
     {
+        public string CollisionTag;
+        public float DamageInterval;
+
+        private Damageable _closeTarget;
+        private float _damageTimer;
+
         new void Start()
         {
             base.Start();
-            Debug.Log("Init EnemyBehaviour");
+            _damageTimer = DamageInterval;
         }
 
-        void Update()
+        private void Update()
         {
+            if (_closeTarget)
+            {
+                _damageTimer += Time.deltaTime;
 
+                if (_damageTimer >= DamageInterval)
+                {
+                    _closeTarget.TakeDamage(10);
+                    _damageTimer = 0;
+                }
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag(CollisionTag))
+            {
+                if (collision.gameObject.TryGetComponent<Damageable>(out var damageable))
+                {
+                    _closeTarget = damageable;
+                }
+            }
+        }
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag(CollisionTag))
+            {
+                _closeTarget = null;
+            }
         }
     }
-
 
 }
