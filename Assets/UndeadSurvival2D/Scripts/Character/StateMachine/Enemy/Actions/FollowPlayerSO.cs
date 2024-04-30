5ed1cee004223ac8ae3b2df123fec22e29024cf3
JousenD <romero87_2112@hotmail.com>
2024-05-01 00:02:25 +0200

    12. Collisions implemented

--- Assets/UndeadSurvival2D/Scripts/Character/StateMachine/Enemy/Actions/FollowPlayerSO.cs
+++ Assets/UndeadSurvival2D/Scripts/Character/StateMachine/Enemy/Actions/FollowPlayerSO.cs
@@ -8,19 +8,20 @@ using JousenD.UndeadSurvival2d.Manager;
     menuName = "StateMachine/Enemy/Actions/Follow Player")]
 public class FollowPlayerSO : StateActionSO
 {
+    public float SpeedModifier;
+    public float DistanceToKeep;
+
     public override StateAction CreateAction()
     {
         return new FollowPlayer();
     }
 }
-
 public class FollowPlayer : StateAction
 {
+    public FollowPlayerSO OriginSO => (FollowPlayerSO)originSO;
     public Vector3 PlayerPosition => GameManager.Instance.GetPlayer().transform.position;
     public Vector3 MyPosition => _myTransform.position;
-
     private Transform _myTransform;
-
     public override void Awake(StateMachineCore stateMachine)
     {
         _myTransform = stateMachine.transform;
@@ -29,15 +30,19 @@ public class FollowPlayer : StateAction
     public override void OnExit() { }
     public override void OnUpdate()
     {
-             RunAction();
+        RunAction();
     }
 
     private void RunAction()
     {
+        if (Vector3.Distance(MyPosition, PlayerPosition) <= OriginSO.DistanceToKeep)
+        {
+            return;
+        }
         _myTransform.position = Vector3.MoveTowards(
             MyPosition,
             PlayerPosition,
-            Time.deltaTime
+            Time.deltaTime * OriginSO.SpeedModifier
         );
     }
 }
