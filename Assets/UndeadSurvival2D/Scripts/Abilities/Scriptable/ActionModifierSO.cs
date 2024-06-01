e9a4c553ab738dbd4f25bda884ecc2e1db40cb2f
JousenD <romero87_2112@hotmail.com>
2024-06-01 20:00:34 +0200

    35. Action Modifier

---
+++ Assets/UndeadSurvival2D/Scripts/Abilities/Scriptable/ActionModifierSO.cs
@@ -0,0 +1,20 @@
+using JousenD.UndeadSurvival2d.Abilities.Action;
+using UnityEngine;
+
+namespace JousenD.UndeadSurvival2d.Abilities.Scriptable
+{
+    public abstract class ActionModifierSO : ScriptableObject
+    {
+        public virtual ActionModifier GetActionModifier(AbilityAction action)
+        {
+            var modifier = CreateActionModifier();
+
+            modifier.Awake(action);
+            modifier.originSO = this;
+
+            return modifier;
+        }
+
+        public abstract ActionModifier CreateActionModifier();
+    }
+}
